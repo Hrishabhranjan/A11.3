3153c9b073701ace0aecd729575c8496c69625ad
Hrishabh ranjan <hrishabhvik@gmail.com>
2025-06-27 13:03:19 +0530

    first commit

--- app.js
+++ app.js
@@ -1,175 +1,93 @@
-// We're finally using Passport.js
-require('dotenv').config();
-const express = require('express');
+const express =require('express');
 const bodyParser = require('body-parser');
-const ejs = require('ejs');
-const mongoose = require('mongoose');
+const ejs = require("ejs");
+
 const session = require('express-session');
-const passport = require('passport');
-const passportLocalMongoose = require('passport-local-mongoose');
 
 
 
-const app = express();
 
 
-
-app.use(express.static("public"));
-app.set("view engine", "ejs");
-
-
-app.use(bodyParser.urlencoded({extended: true}));
-
-
-// Setting up the session
+var app =express();
+app.set("view engine","ejs");
+app.use(express.urlencoded({extended:true}));
+app.use(express.static('public'));
 app.use(session({
-    secret: "Our little secret.", // We'll later save it to our .env file
+    secret: "MySecretKey",
     resave: false,
     saveUninitialized: false
 }));
-
-
-// Initializing the passport and session
-app.use(passport.initialize());
-app.use(passport.session());
-
-
-//mongoose.connect("mongodb://localhost:27017/userDB", 
-
-//);
-
-
-const userSchema = new mongoose.Schema ({
+const mongoose = require ("mongoose");
+mongoose.connect("mongodb://localhost:27017/secrets");
+const trySchema = new mongoose.Schema({
     email: String,
-    password: String,
-    secret: String
+    password: String
 });
-
-
-// Incorporating passport-local-mongoose package. It helps in salting and hashing part. 
-userSchema.plugin(passportLocalMongoose);
-
-
-const User = new mongoose.model("User", userSchema);
-
-
-passport.use(User.createStrategy()); // Used for authenticating users using their username and password.
-passport.serializeUser(User.serializeUser()); // Used for serializing the message and storing it inside a cookie.
-passport.deserializeUser(User.deserializeUser()); // Used for deserializing the message and allowing the message to be discovered.
-
-
-
-app.get("/", (req,res) =>{
-    res.render("home")
+const Item =mongoose.model("second",trySchema);
+app.get("/",function(req,res){
+    res.render("home");
 });
+app.post("/register", async function(req, res) {
+    try {
+        const newuser = new Item({
+            email: req.body.username,
+            password: req.body.password
+        });
 
-
-app.get("/login", (req,res) =>{
-    res.render("login");
+        await newuser.save(); 
+        res.render("secrets");
+    } catch (err) {
+        console.error(err);
+        res.send("Error registering user.");
+    }
 });
+app.post("/login", async function(req, res) {
+    try {
+        let username = req.body.username;
+        let password = req.body.password;
 
+        const foundUser = await Item.findOne({ email: username });
 
-app.get("/register", (req,res) =>{
-    res.render("register");
-});
-
-
-app.get("/secrets", (req, res) => {
-    User.find({ "secret": { $ne: null } })
-      .then(foundUsers => {
-        if (foundUsers) {
-          res.render("secrets", { usersWithSecrets: foundUsers });
+        if (foundUser && foundUser.password === password) {
+            res.render("secrets");
+        } else {
+            res.send("Invalid credentials.");
         }
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).send("An error occurred while finding users with secrets.");
-      });
-  });
-  
+    } catch (err) {
+        console.error(err);
+        res.send("Login error occurred.");
+    }
+});
+req.session.user = foundUser._id;
 
 
-app.get("/submit", (req,res) =>{
-    if(req.isAuthenticated()){
-        res.render("submit");
+app.get("/secrets", function(req, res){
+    if (req.session.user) {
+        res.render("secrets");
     } else {
         res.redirect("/login");
     }
 });
 
 
-app.get("/logout", (req,res) =>{
-    req.logout((err) =>{
-        if(err){
-            console.log(err);
-        }
-    });
-    res.redirect("/");
+
+app.get("/login",function(req,res){
+res.render("login");
 });
-
-
-app.post("/submit", (req, res) => {
-    const submittedSecret = req.body.secret;
-  
-    console.log(req.user.id);
-  
-    User.findById(req.user.id)
-      .then(foundUser => {
-        foundUser.secret = submittedSecret;
-        return foundUser.save(); // Return a Promise here
-      })
-      .then(() => {
-        res.redirect("/secrets");
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).send("An error occurred while saving the secret.");
-      });
-  });
-  
-
-
-app.post("/register", (req,res) =>{
-
-   User.register({username: req.body.username}, req.body.password, function(err, user){
-    if(err){
-        console.log(err);
-        res.redirect("/register");
-    } else {
-        passport.authenticate("local")(req,res, () =>{
-            res.redirect("/secrets");
-        });
-    }
-   });
-    
+app.get("/register",function(req,res){
+res.render("register");
 });
-
-
-app.post("/login", (req,res) =>{
-   
-    const user = new User({
-        username: req.body.username,
-        password: req.body.password
-    });
-
-    // Now we'll use passport.js to authenticate the user.
-    req.login(user, (err) =>{
+app.get("/logout", function(req, res){
+    req.session.destroy(function(err){
         if(err){
             console.log(err);
         } else {
-            passport.authenticate("local")(req,res, function(){
-                res.redirect("/secrets");
-            });
+            res.redirect("/login");
         }
     });
 });
 
 
-
-
-
-port = process.env.PORT || 8000;
-
-app.listen(port, (res,req)=>{
-    console.log(`Server started at ${port}!`);
+app.listen(8000,function(){
+    console.log("Server Started");
 });
